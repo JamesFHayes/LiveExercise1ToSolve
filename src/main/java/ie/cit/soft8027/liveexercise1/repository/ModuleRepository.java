a48ce2b987e65262c6f1e858ee1c0a2eb631bb80
James Hayes <james.hayes1@mycit.ie>
2017-10-04 14:53:41 +0100

    Ive changed a few things here to test git

---
+++ src/main/java/ie/cit/soft8027/liveexercise1/repository/ModuleRepository.java
@@ -0,0 +1,14 @@
+package ie.cit.soft8027.liveexercise1.repository;
+
+import java.util.List;
+
+import ie.cit.soft8027.liveexercise1.entity.Module;
+
+public class ModuleRepository {
+
+	public List<Module> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- src/main/java/ie/cit/soft8027/liveexercise1/service/LearningServiceImpl.java
+++ src/main/java/ie/cit/soft8027/liveexercise1/service/LearningServiceImpl.java
@@ -3,6 +3,7 @@ package ie.cit.soft8027.liveexercise1.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import ie.cit.soft8027.liveexercise1.entity.Module;
@@ -10,23 +11,23 @@ import ie.cit.soft8027.liveexercise1.repository.LearningOutcomeRepository;
 import ie.cit.soft8027.liveexercise1.repository.ModuleRepository;
 
 // MISSING!
-public class LearningServiceImpl // MISSING!
-
-	// MISSING!
+public class LearningServiceImpl {
+	private Module module;
+	
+	@Autowired
 	private ModuleRepository moduleRepo;
 
-	// MISSING!
+	
 	private LearningOutcomeRepository loRepo;
 	
 	/**
 	 * This may not be the most efficient way to do this - using a join query with result set extractor might be faster
 	 */
-	@Override
+	@Autowired
 	public void populateLearningOutcomes(Module module) {
 		module.setLearningOutcomes(loRepo.getByModuleId(module.getId()));
 	}
 
-	@Override
 	public List<Module> getAllModules() {
 		return moduleRepo.findAll();
 	}
